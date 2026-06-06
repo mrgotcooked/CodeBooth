7b0013a3727707592a02cd85174a07e49bb2d4c5
advitya <advitya9@gmail.com>
2026-06-06 10:58:56 +0530

    import fixes

--- frontend/src/Pages/ProblemPage.jsx
+++ frontend/src/Pages/ProblemPage.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
-import { PROBLEMS } from "../data/problems";
-import Navbar from "../components/Navbar";
+import { PROBLEMS } from "../Data/problems";
+import Navbar from "../Components/Navbar";
 
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
-import ProblemDescription from "../components/ProblemDescription";
-import OutputPanel from "../components/OutputPanel";
-import CodeEditorPanel from "../components/CodeEditorPanel";
+import ProblemDescription from "../Components/ProblemDescription";
+import OutputPanel from "../Components/OutputPanel";
+import CodeEditorPanel from "../Components/CodeEditorPanel";
 // import { executeCode } from "../lib/piston";
 
 import toast from "react-hot-toast";
